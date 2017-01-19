d64df9087ce424a598e56caef10616f7728ab5a9
Michael Wheeler <michael@michael-wheeler.org>
2017-01-19 22:16:32 +0000

    prettied

--- index.js
+++ index.js
@@ -1,100 +1,137 @@
-var topic="owntracks/#"
+var topic = "owntracks/#"
 var mqtt = require('mqtt')
 var autobahn = require('autobahn');
 var mqttoptions = require('./mqttoptions.js');
-var client  = mqtt.connect('mqtts://localhost:8883', mqttoptions)
+var client = mqtt.connect('mqtts://localhost:8883', mqttoptions)
 var request = require('request');
 var url = require("url");
 var stackptrurl = "https://stackptr.com/update"
-var base64 = require('node-base64-image'); 
+var base64 = require('node-base64-image');
 
 var stackptrUsers = {}
 
-client.on('connect', function () {
-  client.subscribe(topic)
-})
-
- 
-client.on('message', function (topic, message) {
-  // message is Buffer
-var devId = topic.split("/").slice(-1)[0]
-var user = topic.split("/").slice(-2)[0]
- var locobject = JSON.parse(message.toString())
- updatePtr(devId, locobject.lat, locobject.lon, locobject.batt )
- if (!(devId in stackptrUsers) && devId.length == 32){
-    stackptrUsers[devId] = createWS(devId, user);    
- }
+client.on('connect', function() {
+    client.subscribe(topic)
 })
 
 
-function updatePtr(apikey, lat,lon,battery){
-	var ext = {bat: battery/100}
-        var data = {form:{apikey:apikey,lat:lat, lon:lon, ext: JSON.stringify(ext) }}
-	request.post(stackptrurl, data, function(err,httpResponse,body){
-	})
-
-}
-function loginToWamp(key){
-return function (event, data){
-console.log(key);
-				return(key);
-}
-}
-
-function sendCard(user, fakename, data){
-   var card = {
-  "_type": "card",
-  "name": fakename
-  }
-var parts = url.parse(data, true);
-parts.query.s = 40;
-delete parts.search;
-console.log(url.format(parts))
-
-base64.encode(url.format(parts), {string: true}, function(err,data){
-card.face=data;
-client.publish('owntracks/'+user+'/'+fakename+"/info", JSON.stringify(card), {retain:true});
-
+client.on('message', function(topic, message) {
+    // message is Buffer
+    var devId = topic.split("/").slice(-1)[0]
+    var user = topic.split("/").slice(-2)[0]
+    var locobject = JSON.parse(message.toString())
+    updatePtr(devId, locobject.lat, locobject.lon, locobject.batt)
+    if (!(devId in stackptrUsers) && devId.length == 32) {
+        stackptrUsers[devId] = createWS(devId, user);
+    }
 })
 
+
+function updatePtr(apikey, lat, lon, battery) {
+    var ext = {
+        bat: battery / 100
+    }
+    var data = {
+        form: {
+            apikey: apikey,
+            lat: lat,
+            lon: lon,
+            ext: JSON.stringify(ext)
+        }
+    }
+    request.post(stackptrurl, data, function(err, httpResponse, body) {})
+
 }
 
-function updateUsers(user,data2, sendc){
-console.log(data2)
-console.log("Faking data for " + user + " on owntracks")
-console.log()
-for (var i=0; i<data2.msg.length; i++){
-var content = data2.msg[i];
-console.log(content.loc)
-console.log(content.extra)
-fakeOwnTracks(user, content.username, {"_type":"location",lat: content.loc[0], lon: content.loc[1],tid:content.username.substring(0,2),t:"a", batt:content.extra.bat*100,tst:content.lastupd,conn:"w"})
-if (sendc) { sendCard(user, content.username, content.icon)}
-console.log(content.username)
-}
+function loginToWamp(key) {
+    return function(event, data) {
+        console.log(key);
+        return (key);
+    }
 }
 
-function fakeOwnTracks(user, fakename, data){
-console.log('owntracks/'+user+'/'+fakename)
-console.log(data)
-client.publish('owntracks/'+user+'/'+fakename, JSON.stringify(data), {retain:true});
+function sendCard(user, fakename, data) {
+    var card = {
+        "_type": "card",
+        "name": fakename
+    }
+    var parts = url.parse(data, true);
+    parts.query.s = 40;
+    delete parts.search;
+    console.log(url.format(parts))
+
+    base64.encode(url.format(parts), {
+        string: true
+    }, function(err, data) {
+        card.face = data;
+        client.publish('owntracks/' + user + '/' + fakename + "/info", JSON.stringify(card), {
+            retain: true
+        });
+
+    })
+
 }
 
-function createWS(key, user){
-   console.log(user)
-	var connection = new autobahn.Connection({url: 'wss://stackptr.com/ws', realm: 'stackptr',authid: '-1',authmethods: ["ticket"], onchallenge: loginToWamp(key)});
-
-connection.onopen = function (session) {
-console.log("websocket opened");
-
-session.subscribe("com.stackptr.user", function(data1,data2){return updateUsers(user,data2)});
-session.call('com.stackptr.api.userList' ).then(function(data) {updateUsers(user,{"msg": data[1].data} , true   )
-});
+function updateUsers(user, data2, sendc) {
+    console.log(data2)
+    console.log("Faking data for " + user + " on owntracks")
+    console.log()
+    for (var i = 0; i < data2.msg.length; i++) {
+        var content = data2.msg[i];
+        console.log(content.loc)
+        console.log(content.extra)
+        fakeOwnTracks(user, content.username, {
+            "_type": "location",
+            lat: content.loc[0],
+            lon: content.loc[1],
+            tid: content.username.substring(0, 2),
+            t: "a",
+            batt: content.extra.bat * 100,
+            tst: content.lastupd,
+            conn: "w"
+        })
+        if (sendc) {
+            sendCard(user, content.username, content.icon)
+        }
+        console.log(content.username)
+    }
 }
-connection.onclose = function(reason,details){
-console.log("///")
-console.log(reason);
-console.log(details);
-console.log("\\\\\\");
+
+function fakeOwnTracks(user, fakename, data) {
+    console.log('owntracks/' + user + '/' + fakename)
+    console.log(data)
+    client.publish('owntracks/' + user + '/' + fakename, JSON.stringify(data), {
+        retain: true
+    });
 }
-connection.open();
+
+function createWS(key, user) {
+    console.log(user)
+    var connection = new autobahn.Connection({
+        url: 'wss://stackptr.com/ws',
+        realm: 'stackptr',
+        authid: '-1',
+        authmethods: ["ticket"],
+        onchallenge: loginToWamp(key)
+    });
+
+    connection.onopen = function(session) {
+        console.log("websocket opened");
+
+        session.subscribe("com.stackptr.user", function(data1, data2) {
+            return updateUsers(user, data2)
+        });
+        session.call('com.stackptr.api.userList').then(function(data) {
+            updateUsers(user, {
+                "msg": data[1].data
+            }, true)
+        });
+    }
+    connection.onclose = function(reason, details) {
+        console.log("///")
+        console.log(reason);
+        console.log(details);
+        console.log("\\\\\\");
+    }
+    connection.open();
 }
